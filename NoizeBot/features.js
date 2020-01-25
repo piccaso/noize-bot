a774bdd61141a94f1e7b1aaf2fa8eea860f90d5a
ff <>
2020-01-25 02:20:45 +0100

    add some fun

--- NoizeBot/features.js
+++ NoizeBot/features.js
@@ -26,18 +26,25 @@ match(/:birthday:|geburtstag|birthday/i, './tracks/happyBirthday.mp3');
 match(/stress/i, './tracks/stress.mp3');
 match(/party/i, './tracks/waynesworld.mp3');
 match(/aaa/i, './tracks/wilhelmScream.mp3');
+match(/hare|krishna|Kṛṣṇa|Krsna|pray|:innocent:/i, 'tracks/hareKrishna.mp3');
+match(/hardcore/i, 'tracks/hardcore.mp3');
+match(/egal|:relaxed:/i, 'tracks/egal.mp3');
+match(/:middle_finger:/i, 'tracks/fea.mp3');
+match(/bathroom|toilet/i, 'tracks/bathroom.mp3');
+match(/balls|nukem/i, 'tracks/ballsOfSteel.mp3');
+match(/fake news/i, 'tracks/fakeNews.mp3');
+match(/get back to work|slacker/i, 'tracks/slacker.mp3');
+match(/Schlafn|Schlofn|Hackln/i, 'tracks/hackln.mp3');
 
 
-// Smileys Remote:
-match(/hare|krishna|pray|:innocent:/i, 'http://dl.prokerala.com/downloads/ringtones/files/mp3/hare-krishna-48971.mp3');
-
-// Commands
-match(/^say (.*)$/, m => run("/bin/bash", ['-c', `echo "${m[1].replace('"', '\\"')}" | festival --tts`]));
-match(/^play_url (http[^ ]+)$/, m => run('/bin/bash', ['-c', `curl "${m[1].replace('"','\\"')}" | mpg123 -`]));
-match(/^fart_attack ([1-9]{1})/, m => {
+// Commands (wip)
+match(/^nb_say (.*)$/, m => run("/bin/bash", ['-c', `echo "${m[1].replace('"', '\\"')}" | festival --tts`]));
+match(/^nb_play_url (http[^ ]+)$/, m => run('/bin/bash', ['-c', `curl "${m[1].replace('"','\\"')}" | mpg123 -`]));
+match(/^nb_fart_attack ([1-9]{1})/, m => {
     var playlist = [];
     for (var i = 0; i < m[1]; i++) {
         playlist.push('./tracks/smileys/hankey.mp3');
     }
+    playlist.push('./tracks/smileys/smile.mp3');
     run("mpg123", playlist);
 });
