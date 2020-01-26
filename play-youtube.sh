04fe50340af8751d557dd093c7c59dfd45930779
ff <>
2020-01-26 23:46:17 +0100

    try wav

--- play-youtube.sh
+++ play-youtube.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 tmp_dir=$(mktemp -d -t yt-XXXXXXXXXX)
 cd $tmp_dir
-youtube-dl -x --audio-format mp3 "$@"
-mpg123 *.mp3
+youtube-dl -x --audio-format wav "$@"
+aplay *.mp3
 cd -
 rm -rf $tmp_dir
 
